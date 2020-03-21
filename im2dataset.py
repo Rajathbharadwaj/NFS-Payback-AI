5073631be37b58f4d82c8375f0cb785bb2f00224
Rajath Bharadwaj <rajathrajath940@gmail.com>
2020-03-21 15:00:53 +0530

    Added reverse

--- im2dataset.py
+++
@@ -1,7 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-arguments = {"keywords":"Polar bears,baloons,Beaches","limit":100,"print_urls":True, "format":"jpg", "chromedriver":"C:\\Users\\Rajath\webdriver\\chromedriver"}
-
-path = response.download(arguments)
-print(path)
